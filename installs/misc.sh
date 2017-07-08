56691be1caaec70098123b55bdd35d00be7ccdba
Nicolas Duvieusart Déry <nddery@gmail.com>
2017-07-08 17:02:56 -0400

    Install vim-plug for vim and neovim

--- installs/misc.sh
+++ installs/misc.sh
@@ -3,13 +3,18 @@
 source utils.sh
 
 main() {
-  # Install tmux-vim-select-pane
-  # https://gist.github.com/mislav/5189704
-  if [ ! -f /usr/local/bin/tmux-vim-select-pane ]
+  # vim-plug, for vim
+  if [ ! -f ~/.vim/autoload/plug.vim ]
   then
-    execute "curl -fsSL https://raw.github.com/mislav/dotfiles/1500cd2/bin/tmux-vim-select-pane \
-      -o /usr/local/bin/tmux-vim-select-pane \
-      && chmod +x /usr/local/bin/tmux-vim-select-pane" "Installed tmux-vim-select-pane"
+    execute "curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+  fi
+
+  # vim-plug, for neovim
+  if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]
+  then
+    execute "curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
   fi
 }
 
